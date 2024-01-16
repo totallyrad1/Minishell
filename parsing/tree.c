8fef701d3906fd02f5efd1fc8711febc480a2b41
Yassine Zaazaa <yzaazaa@e1r6p1.1337.ma>
2024-01-16 02:23:42 +0100

    still not done

--- parsing/tree.c
+++ parsing/tree.c
@@ -6,7 +6,7 @@
 /*   By: yzaazaa <yzaazaa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 22:10:15 by yzaazaa           #+#    #+#             */
-/*   Updated: 2024/01/16 00:39:50 by yzaazaa          ###   ########.fr       */
+/*   Updated: 2024/01/16 02:19:32 by yzaazaa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,6 @@ void	make_tree_right(t_tree **tree, t_cmd *cmd)
 				make_tree_right(&((*tree)->right), cmd->next);
 			if (cmd->prev)
 				make_tree_left(&((*tree)->left), cmd->prev);
-			return ;
 		}
 		cmd = cmd->next;
 	}
@@ -143,7 +142,6 @@ void	make_tree_left(t_tree **tree, t_cmd *cmd)
 				make_tree_right(&((*tree)->right), cmd->next);
 			if (cmd->prev)
 				make_tree_left(&((*tree)->left), cmd->prev);
-			return ;
 		}
 		cmd = cmd->prev;
 	}
@@ -177,12 +175,26 @@ int pipe_in_cmd(t_cmd *cmd)
 	return 0;
 }
 
+void rotationDroite(t_tree **T)
+{
+    if(*T==NULL || (*T)->left==NULL)
+        return;
+    t_tree *temp = (*T)->left;
+    (*T)->left = temp->right;
+    temp->right = *T;
+    *T = temp;
+    return;
+}
+
 t_tree	*make_tree(t_cmd *cmd)
 {
-	t_tree	*tree;
-	t_tree	*first_node;
+	t_tree	*first_node_right;
+	t_tree	*first_node_left;
+	t_tree	*right;
+	t_tree	*left;
 
-	tree = NULL;
+	right = NULL;
+	left = NULL;
 	while (cmd->next)
 		cmd = cmd->next;
 	if (and_or_in_cmd(cmd))
@@ -192,22 +204,26 @@ t_tree	*make_tree(t_cmd *cmd)
 			if (cmd->type == TOKEN_AND)
 			{
 				cmd->visited = 1;
-				add_left_child(&tree, cmd, AND);
-				first_node = tree;
+				add_left_child(&right, cmd, AND);
+				add_left_child(&left, cmd, AND);
+				first_node_right = right;
+				first_node_left = left;
 				if (cmd->next)
-					make_tree_right(&tree, cmd->next);
+					make_tree_right(&right, cmd->next);
 				if (cmd->prev)
-					make_tree_left(&tree, cmd->prev);
+					make_tree_left(&left, cmd->prev);
 			}
 			else if (cmd->type == TOKEN_OR)
 			{
 				cmd->visited = 1;
-				add_left_child(&tree, cmd, OR);
-				first_node = tree;
+				add_left_child(&right, cmd, OR);
+				add_left_child(&left, cmd, OR);
+				first_node_right = right;
+				first_node_left = left;
 				if (cmd->next)
-					make_tree_right(&tree, cmd->next);
+					make_tree_right(&right, cmd->next);
 				if (cmd->prev)
-					make_tree_left(&tree, cmd->prev);
+					make_tree_left(&left, cmd->prev);
 			}
 			cmd = cmd->prev;
 		}
@@ -221,12 +237,14 @@ t_tree	*make_tree(t_cmd *cmd)
 				if (cmd->type == TOKEN_PIPE)
 				{
 					cmd->visited = 1;
-					add_left_child(&tree, cmd, PIPE);
-					first_node = tree;
+					add_left_child(&right, cmd, PIPE);
+					add_left_child(&left, cmd, PIPE);
+					first_node_right = right;
+					first_node_left = left;
 					if (cmd->next)
-						make_tree_right(&tree, cmd->next);
+						make_tree_right(&right, cmd->next);
 					if (cmd->prev)
-						make_tree_left(&tree, cmd->prev);
+						make_tree_left(&left, cmd->prev);
 				}
 				cmd = cmd->prev;
 			}
@@ -238,16 +256,22 @@ t_tree	*make_tree(t_cmd *cmd)
 				if (cmd->type == TOKEN_EXPR)
 				{
 					cmd->visited = 1;
-					add_left_child(&tree, cmd, CMD);
-					first_node = tree;
+					add_left_child(&right, cmd, CMD);
+					add_left_child(&left, cmd, CMD);
+					first_node_right = right;
+					first_node_left = left;
 					if (cmd->next)
-						make_tree_right(&tree, cmd->next);
+						make_tree_right(&right, cmd->next);
 					if (cmd->prev)
-						make_tree_left(&tree, cmd->prev);
+						make_tree_left(&left, cmd->prev);
 				}
 				cmd = cmd->prev;
 			}
 		}
 	}
-	return (first_node);
+	t_tree	*head = new_node(first_node_left, first_node_left->tree_type);
+	head->right = first_node_right->right;
+	head->left = first_node_left->left;
+	// rotationDroite(&head);
+	return (head);
 }
